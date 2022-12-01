3fd3e363a9c1279383700cee2da02ba0514f478d
Shannon Cornish <shannon@shannoncornish.com>
2022-11-30 23:54:36 -0600

    Day 1

---
+++ day-01/Program.cs
@@ -0,0 +1,23 @@
+﻿var inputPath = $"{args.FirstOrDefault() ?? "Input"}.txt";
+var input = File.ReadLines(inputPath);
+
+var calories = new List<int> { 0 };
+var i = 0;
+
+foreach (var line in input)
+{
+    if (line == "")
+    {
+        i++;
+        calories.Add(0);
+    }
+    else
+    {
+        calories[i] += int.Parse(line);
+    }
+}
+
+var orderedCalories = calories.OrderDescending();
+
+Console.WriteLine($"Total calories the of the top elf: {orderedCalories.First()}"); // 75501
+Console.WriteLine($"Total calories of the top three elves: {orderedCalories.Take(3).Sum()}"); // 215594
